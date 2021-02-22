574469ed2eb4ed3208a75c089605f4f20a2a2113
Yashvi70 <73021648+Yashvi70@users.noreply.github.com>
2021-02-22 13:59:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, gameState,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(1000, 500);
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  if (gameState === "start") {
+    background("black");
+    textSize(30);
+    fill("teal");
+    text("CRUMPLED PAPER BALL 1 \n Hello Guys!!  \n To play this game please press up arrow key    ", 50, 200)
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+    textSize(30);
+    fill("teal");
+    text("Enjoy your Game!!!!!")
+    dustbin.display();
+    paper.display();
+
+  }
+}
+
+
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 12,
+      y: -13
+    });
+  }
+}
